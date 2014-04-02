46badbaba806a21d11783783ceb6729159f5425e
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2014-04-01 18:56:58 -0700

    imported objects cleaned up

--- wp-content/themes/roots/assets/js/orderform.js
+++ wp-content/themes/roots/assets/js/orderform.js
@@ -1,5 +1,10 @@
 var orderForm = (function($) {
 	var feedJSON;
+	var settings = {
+		container: function() {
+			return $('.app-container');
+		}
+	};
 	var init = function() {
 		getGSData();
 	};
@@ -7,11 +12,16 @@ var orderForm = (function($) {
 		$.ajax({
 			url: 'https://spreadsheets.google.com/feeds/list/0Ase_pp75HN9MdGlpbkZmZmVTUGU2MElkOEktVUxyQWc/od6/public/values?alt=json',
 			success: function(data) {
-				console.log(data);
+				// console.log(data);
 				$(document).ready( function() {
 					feedJSON = data.feed.entry;
-					popFields(feedJSON);
-					console.table(feedJSON);
+					console.table(flattenGSFeed(feedJSON));
+					var properFormObj = groupObjects(flattenGSFeed(feedJSON));
+					console.table(properFormObj);
+					// popFields(feedJSON);
+					// stringJSON(properFormObj);
+					// console.table(properFormObj);
+					// console.table(feedJSON);
 				});
 			},
 			error: function(thiss, that, other) {
@@ -24,12 +34,62 @@ var orderForm = (function($) {
 	var popFields = function(feedJSON) {
 		var strings = JSON.stringify(feedJSON, null, "\t");
 		// $('.app-container').append('<div>'+strings+'</div>');
-		var container = $('.app-container');
 
 		for (var i = 0; i < feedJSON.length; i++) {
-			$(container).append('<div>' + feedJSON[i].gsx$type.$t + ' ' + feedJSON[i].gsx$name.$t + ' ' + feedJSON[i].gsx$rate.$t + '</div>');
+			$(settings.container).append('<div>' + feedJSON[i].gsx$type.$t + ' ' + feedJSON[i].gsx$name.$t + ' ' + feedJSON[i].gsx$rate.$t + '</div>');
 		}
-	}
+	};
+	var stringJSON = function(obj) {
+		$('.app-container').append('<pre>' + JSON.stringify(obj, null, "\t") + '</pre>');
+	};
+	var flattenGSFeed = function(obj) {
+		var output = {};
+		for (var entry in obj) {
+			if (obj.hasOwnProperty(entry)) {
+				output[entry] = {};
+				for (var key in obj[entry]) {
+					if (obj[entry].hasOwnProperty(key)) {
+						if (key.slice(0,4) === "gsx$") {
+							// var someobject;
+							var newname = key.slice(4);
+							output[entry][newname] = obj[entry][key].$t;
+							// console.log(output);
+						}
+					}
+				}
+			}
+		}
+		// console.log(output);
+		return output;
+	};
+	var groupObjects = function(obj) {
+		var output = [];
+		output.push({
+			type: 'uncategorized',
+			items: []
+		});
+		// var entryDepth = 0;
+		for (var entry in obj) {
+			// console.log('entry:' + entry);
+			// console.log('Obj-entry:');
+			// console.log(obj[entry]);
+			if (obj[entry].type === "Section") {
+				console.log("found section");
+				console.log(obj[entry]);
+				output.push({
+					type: obj[entry].name,
+					items: []
+				});
+				// entryDepth++;
+			}
+			else if (obj[entry].type === "Item") {
+				console.log("found item");
+				console.log(obj[entry]);
+				output[output.length - 1].items.push(obj[entry]);
+			}
+		}
+		return output;
+	};
 
 	return {
 		init: init
--- wp-content/themes/roots/templates/content-page-orderform.php
+++ wp-content/themes/roots/templates/content-page-orderform.php
@@ -1,7 +1,50 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
 
-	<div class="app-container"></div>
+	<div class="app-container">
+		<form action="" id="main-form">
+			
+		</form>
+	</div>
+	<div class="templates">
+		<!-- form-section -->
+		<section class="form-section" data-template-item="">
+			<section class="section-header">
+				<h3 class="item-heading" data-value-inner="section-heading"></h3>
+			<div class="section-contents" data-include="section-contents">
+				<header class="table-header">
+					<div class="name">
+						<p class="cell-label" data-perms-client="r" data-value-inner="name"></p>
+					</div>
+					<div class="quantity">
+						<p class="cell-label" data-perms-client="r"></p>
+						<input class="cell-label" data-perms-client="rw" />
+					</div>
+					<div class="rate">
+						<p class="cell-label" data-perms-client="r" data-value-inner="rate"></p>
+					</div>
+					<div class="days-wk" data-default="7"></div>
+					<div class="days" data-input="input"></div>
+					<div class="estimate"></div>
+				</header>
+			</div>
+		</section>
+		<!-- heading -->
+		<div class="heading">
+			<h3 class="item-heading"></h3>
+		</div>
+		<!-- item -->
+		<div data-template-item="item" class="item">
+		</div>
+		<!-- item-inner -->
+		<input data-template-item="item-input" class="cell-input" />
+		<p data-template-item="item-ro" class="cell-ro-value" data-value-inner="true"></p>
+
+
+
+
+
+	</div>
 
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
