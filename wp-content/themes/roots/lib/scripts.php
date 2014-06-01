0ea716170a2a8890238e9225cfc298e094ac87b1
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2014-05-31 19:01:49 -0700

    Hiding swoopy sidebar on smaller screen sizes

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '75891e586327bf24a5e3c26a934e5112');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '25480a4bd04f2107f02d122304be4a02');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
