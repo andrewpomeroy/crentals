44270b0618cdd78da25d7ae6d231ebf21bc2e4ca
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2014-06-11 01:45:00 -0700

    Style media elements for Category pages

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '25480a4bd04f2107f02d122304be4a02');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e43866c4be3c9e5a7cff00f398c8195d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
