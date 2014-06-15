835f0c997a825fc3ed4006c4d787c0b7883befd2
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2014-06-14 21:54:56 -0700

    Header padding on estimate page

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd0c4f2e56ce909f602208249bb0b1fec');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a0009a550a9446cb49c75696c5de55a6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
