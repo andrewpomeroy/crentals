99d9441249b7bb93d4fccb6a63d4576a6b2d33d8
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2014-06-15 18:24:27 -0700

    modal on image list not really working

--- wp-content/themes/roots/templates/content-page-imagelist.php
+++ wp-content/themes/roots/templates/content-page-imagelist.php
@@ -9,14 +9,15 @@ $args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'post_status' =
 				// count number of available images and change if less than the specified limit
 				foreach ($theattachments as $post) {
 						setup_postdata($post);
-						$image = wp_get_attachment_image_src( $post->ID, 'thumbnail', false );
+						$imgthumb = wp_get_attachment_image_src( $post->ID, 'thumbnail', false );
+						$imgsrc = wp_get_attachment_image_src( $post->ID, 'full', false );
 						?>
 						<div class="demo-image">
-							<a class="demo-image__img-wrap" href="<?php echo $image[0]?>" target="_blank">
-								<img class="demo-image__img" src="<?php echo $image[0] ?>">
+							<a class="demo-image__img-wrap" href ng-click="infoModal({name:'<?php echo get_the_title()?>', image: '<?php echo $imgsrc[0] ?>'})">
+								<img class="demo-image__img" src="<?php echo $imgthumb[0] ?>">
 							</a>
 							<p class="demo-image__title"><?php get_the_title() ?></p>
-							<textarea class="demo-image__url copy-this-url" onclick="this.focus();this.select()" readonly="readonly"><?php $imgsrc = wp_get_attachment_image_src( $post->ID, 'full', false ); echo $imgsrc[0] ?></textarea>
+							<textarea class="demo-image__url copy-this-url" onclick="this.focus();this.select()" readonly="readonly"><?php echo $imgsrc[0] ?></textarea>
 							<a class="demo-image__admin-link" href="<?php echo get_site_url() ?>/wp-admin/post.php?post=<?php echo $post->ID ?>&action=edit">Admin link</a>
 						</div>
 						<?php
