fd6c79424ec87ca44d5637c380c32e87a6ff104c
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2014-07-20 18:12:46 -0700

    File list error undefined variable fixed

--- wp-content/themes/roots/templates/content-file-list.php
+++ wp-content/themes/roots/templates/content-file-list.php
@@ -1,16 +1,9 @@
 <?php // create $mhfiles array ?>
- 	<?php
- 	if (get_field('enable_files')) {
-		$mhfiles = get_field('files');
-	}
- 	?>
-	
-<?php // Display File Listing - Pdf_16x16_Crystal_SVG.png ?>	
-
-<?php if ( $mhfiles ) { 
-
+<?php
+ if (get_field('enable_files')) {
+	$mhfiles = get_field('files');
 	$filecount = 0;
-		?>
+	?>
 	<div class="work-files-list work-content-list">
 		<h3 id="files_list_link" class="files-list-link content-list-link"><?php echo get_field('files_heading') ?> (<?php echo count($mhfiles); ?>)</h3>
 		<ol id="project-files" class="project-files project-content" class="c">
