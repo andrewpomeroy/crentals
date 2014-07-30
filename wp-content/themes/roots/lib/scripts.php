8afd0e31e41cc3c49d60f771acc098bedda4d7e7
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2014-07-30 01:04:57 -0700

    debug on

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4ea36ef6eb39c7e269283aad07ce77f1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dafd342e7145089722ad3d4b48f9a54c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
