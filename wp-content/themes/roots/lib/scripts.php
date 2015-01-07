1c48b2c084dfc4767724aa47a9efc4552bc03519
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2015-01-07 01:22:03 -0800

    genericized .estimate-form selector (different slug up on production instance)

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c20ed4f5a6545ac654a7d68e1d3df32f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '83ae3d39322c83ad1a4f3a83e3674a75');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
